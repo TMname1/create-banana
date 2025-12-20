0041d9013fa878758a7a9dfa098f25d3fd5f4928
TMname1 <tmname0@gmail.com>
2025-12-20 18:33:15 +0800

    feat: add .eslintignore file for eslint feat in template

--- utils/template/Eslint.js
+++ utils/template/Eslint.js
@@ -5,9 +5,12 @@ import { templatePath } from '../URL.js'
 
 const addEslint = async (projectName) => {
   // 路径
+  const templateEslintPath = path.join(templatePath, 'Eslint')
+  const targetPath = path.join(process.cwd(), projectName)
+  // 文件
   const basePackageJsonPath = path.join(templatePath, 'base', 'package.json')
-  const eslintPackageJsonPath = path.join(templatePath, 'Eslint', 'package.json')
-  const targetPackageJsonPath = path.join(process.cwd(), projectName, 'package.json')
+  const eslintPackageJsonPath = path.join(templateEslintPath, 'package.json')
+  const targetPackageJsonPath = path.join(targetPath, 'package.json')
 
   // 读取package.json文件
   const basePkg = await fs.readJson(basePackageJsonPath)
@@ -19,12 +22,22 @@ const addEslint = async (projectName) => {
     ...eslintPkg.devDependencies,
   }
 
+  basePkg.scripts = {
+    ...basePkg.scripts,
+    ...eslintPkg.scripts,
+  }
+
   await fs.writeJson(targetPackageJsonPath, basePkg, { spaces: 2 })
 
   // 复制Eslint配置文件
-  const eslintConfigPath = path.join(templatePath, 'Eslint', 'eslint.config.js')
-  const eslintConfigTargetPath = path.join(process.cwd(), projectName, 'eslint.config.js')
+  const eslintConfigPath = path.join(templateEslintPath, 'eslint.config.js')
+  const eslintConfigTargetPath = path.join(targetPath, 'eslint.config.js')
   fs.copyFileSync(eslintConfigPath, eslintConfigTargetPath)
+
+  // 复制.eslintignore文件
+  const eslintIgnorePath = path.join(templateEslintPath, '.eslintignore')
+  const eslintIgnoreTargetPath = path.join(targetPath, '.eslintignore')
+  fs.copyFileSync(eslintIgnorePath, eslintIgnoreTargetPath)
 }
 
 export default addEslint
